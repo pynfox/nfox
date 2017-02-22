53eaa1b9ca93de9269a39a2c30ec68f4b52b15f4
Labrys <Labrys.git@gmail.com>
2017-02-21 19:00:35 -0500

    Add nfox package

---
+++ nfox/__init__.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+
+"""
+NFox Media Information Utility
+
+Create NFO files for your video collection.
+"""
+
+VERSION = (0, 0, 0)  # version as tuple for simple comparisons
+__version__ = '.'.join(map(str, VERSION))
